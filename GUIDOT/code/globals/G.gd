b9b7b53e1e2d790a004b262fb6a1f5dd080f9dbd
SigularityFuel <314410399+SingularityFuel@users.noreply.github.com>
2026-08-19 22:11:10 -0600

    ++

---
+++ GUIDOT/code/globals/G.gd
@@ -0,0 +1,6 @@
+# Set as a Global Autoload in project
+# so it can be accessed from anywhere.
+
+# G Class Global State
+
+extends Node
---
+++ GUIDOT/code/globals/S.gd
@@ -0,0 +1,6 @@
+# Set as a Global Autoload in project
+# so it can be accessed from anywhere.
+
+# S Class Signal Nexus
+
+extends Node
