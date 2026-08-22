94d2be0e22dee86c7b3083682e7209de68454551
SingularityFuel <314410399+SingularityFuel@users.noreply.github.com>
2026-08-22 14:46:18 -0600

    relabel class buttons (#4)

---
+++ GUIDOT/code/components/relabel.gd
@@ -0,0 +1,13 @@
+extends ReColor
+
+@export var L: Label
+@export var text: String = "---"
+
+func _ready() -> void:
+	ReLabel(color,text)
+
+func ReLabel(c:Color,t:String) -> void:
+	if L != null && t != null:
+		text = t
+		L.text = t
+	ReColor(c)
