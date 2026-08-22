ee31480a7f48b9ec9ab25344eb49a67550abec6a
SigularityFuel <314410399+SingularityFuel@users.noreply.github.com>
2026-08-22 16:27:54 -0600

    ++

--- GUIDOT/code/globals/S.gd
+++ GUIDOT/code/globals/S.gd
@@ -4,3 +4,12 @@
 # S Class Signal Nexus
 
 extends Node
+
+signal NumKey(i:int)
+signal AbcKey(k:String)
+signal OddKey(k:String)
+
+func _init() -> void:
+	NumKey.connect(L.logNumKey)
+	AbcKey.connect(L.logAbcKey)
+	OddKey.connect(L.logOddKey)
